d9d5637ae7acef7dbf4589e1ebb00c354d49b518
Mohith H B <mohithhb@gmail.com>
2023-11-12 23:27:24 +0530

    change of baseurl

--- ante-app/src/Constant.js
+++ ante-app/src/Constant.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://ante-api-project.onrender.com/";
+export const BASE_URL = "https://ante-media.netlify.app/";
 
 function replaceUrl(imageUrl) {
   imageUrl = imageUrl.replace(/\\/g, "/");
